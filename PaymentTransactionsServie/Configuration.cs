99b72428099756edf911c438c071c61f5c1b6f11
Ruslan Yevmenkin <iamtheru@gmail.com>
2023-02-16 11:21:07 +0200

    add Nlog, ConfigService

---
+++ PaymentTransactionsServie/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentTransactionsServie
+{
+	public class Configuration
+	{
+		public string SourcePath { get; set; }
+
+		public string DestinationPath { get; set; }
+
+		public string AllowedFilesFormats { get; set; }
+	}
+}
